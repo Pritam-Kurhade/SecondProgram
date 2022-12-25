11383cc158e2e2e94a934c31bb9139476bf62815
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-25 14:50:02 +0530

    First code: arraylist and linkedlist

---
+++ src/com/velocity/git/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.velocity.git;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<String> list= new LinkedList<String>();
+		list.add("RR");
+		list.add("KK");
+		list.add("PP");
+		System.out.println(list);
+	}
+}
