ca6c0c2180b5c03b3bbd763a41cb5a0f44ca1050
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-25 14:52:10 +0530

    Second code: VectorDemo

---
+++ src/com/velocity/git/VectorDemo.java
@@ -0,0 +1,13 @@
+package com.velocity.git;
+
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		Vector<Integer> v= new Vector<Integer>();
+		v.add(55);
+		v.add(77);
+		System.out.println(v);
+	}
+}
