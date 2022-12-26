3a15871caf56ebc773613abd7dbb8f4fd71aeb58
kurhadep06@gmail.com <kurhadep06@gmail.com>
2022-12-26 13:28:49 +0530

    hashset code

---
+++ src/com/velocity/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set= new HashSet<>();
+		set.add(2);
+		set.add(5);
+		set.add(55);
+		System.out.println(set);
+	}
+}
